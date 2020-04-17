cb0f8ce8094501e16a90677fd20a526ea23ebf5b
Sugeyth <00143619@uca.edu.sv>
2020-04-17 13:20:29 -0600

    Add: Document Class

---
+++ Parcial 01/src/com/DSUR/x00143619/Documento.java
@@ -0,0 +1,19 @@
+package com.DSUR.x00143619;
+
+public class Documento {
+    private String Nombre;
+    private String Numero;
+
+    public Documento(String nombre, String numero) {
+        this.Nombre = nombre;
+        this.Numero = numero;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getNumero() {
+        return Numero;
+    }
+}
